37d997f66280eade74dd81ebecc05cfb4b5d14fd
zhao liang <zhaoliang@zhaodeMac-mini.local>
2015-06-08 18:55:02 +0800

    Initial Commit

---
+++ iOSSnapChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSSnapChat
+//
+//  Created by zhao liang on 15/6/8.
+//  Copyright (c) 2015年 gongtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSSnapChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSSnapChat
+//
+//  Created by zhao liang on 15/6/8.
+//  Copyright (c) 2015年 gongtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
